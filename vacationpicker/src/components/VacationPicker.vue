82d1ee98df90cdea9aacd9bebd51836a756147fa
GRamdjiawan <302325196@student.rocmondriaan.nl>
2022-06-06 00:09:00 +0200

    hoofdstuk 4 gemaakt

--- vacationpicker/src/components/VacationPicker.vue
+++ vacationpicker/src/components/VacationPicker.vue
@@ -27,8 +27,11 @@
     </div>
 
     <div class="row ">
-      <div class="col-4 my-5">
-        <ul class="list-group " v-for="(country, index) in countryData.countries" v-bind:key="country.id"
+
+
+
+      <div class="col-6 my-5">
+<!--        <ul class="list-group " v-for="(country, index) in countryData.countries" v-bind:key="country.id"
              @click="selectCountry(index)">
           <li class="list-group-item"  v-bind:title="country.details" v-bind:id="country.id">
             <span v-bind:id="country.id" v-bind:title="country.details">
@@ -36,11 +39,30 @@
             </span>
 
           </li>
-        </ul>
+        </ul>-->
+
+
+        <div>
+            <h2 class="display-6 bg-warning bg-opacity-25 p-3 text-center">
+              {{selectedCountry.name}}
+              <br>
+              Capital: <b>{{selectedCountry.capital}}</b>
+              <br>
+              <span v-if="onSale" class="text-dark">
+                <hr>
+
+                ON SALE!!
+              </span>
+
+            </h2>
+          <button @click="selectedCountryIndex--" class="btn btn-danger">Back</button>
+          <button @click="selectedCountryIndex++" class="btn btn-success">Next</button>
+          <button @click="hideDetails" class="btn btn-warning">Hide details</button>
+        </div>
       </div>
 
 
-      <div class="col-4 my-4">
+      <div class="col-4 my-6">
         <h2>{{ selectedCountry.name }}</h2>
         <ul class="list-group" >
           <li class="list-group-item">
@@ -52,12 +74,13 @@
           <li class="list-group-item">
             {{ selectedCountry.capital }}
           </li>
-          <li class="list-group-item">
-            {{ selectedCountry.details }}
-          </li>
+
           <li class="list-group-item">
             <img :src=" getImgUrl(selectedCountry.img)" :alt="selectedCountry.img" class="img-fluid">
           </li>
+          <li class="list-group-item" v-if="hideDetail == true">
+          {{ selectedCountry.details }}
+        </li>
           <li class="list-group-item" v-show="onSale">
             <p class="bg-warning px-2">On Sale!</p>
             <p>{{ text}}</p>
@@ -65,7 +88,7 @@
         </ul>
       </div>
 
-      <div class="col-md-4 ">
+<!--      <div class="col-md-4 ">
         <h2 class="mt-3">Destinations cheaper than:</h2>
         <select class="form-control-sm" v-model="selectedCost"  @change="filterCountries()">
           <option v-for="(cost, index) in costs" :key="index" :value="cost">
@@ -78,13 +101,14 @@
             {{country.name}} (EUR: {{country.cost}})
           </li>
         </ul>
-      </div>
+      </div>-->
     </div>
 
     <div class="row border-top border-dark">
       <div class="col-md-4 mt-2">
         <h2>Add country</h2>
         <input type="text" v-model="newCountry" @keyup.enter="addCountry(newCountry)" class="form-control" placeholder="Add a new country">
+        <br>
         <button @click="addCountry(newCountry)" class="btn btn-outline-dark">Add country</button>
 
 
@@ -130,6 +154,7 @@ export default {
       newCountry: '',
       newCountries: [],
 
+      hideDetail: true,
 
     }
   },
@@ -146,11 +171,16 @@ export default {
       this.counter--;
     },
 
+    hideDetails() {
+      return this.hideDetail = false;
+    },
+
     selectCountry(index) {
       this.selectedCountryIndex = index;
       console.log(this.selectedCountryIndex);
 
     },
+
 
     filterCountries() {
       this.filteredCountries = this.countryData.countries.filter(country => country.cost < this.selectedCost)
@@ -166,6 +196,7 @@ export default {
     selectedCountry() {
 
       return {
+
         id: this.countryData.countries[this.selectedCountryIndex].id,
         name: this.countryData.countries[this.selectedCountryIndex].name,
         capital: this.countryData.countries[this.selectedCountryIndex].capital,
@@ -202,7 +233,7 @@ h1 {
   font-weight: bold;
 }
 
-.countries {
+/*countries {
   width: 100%;
   font-size: 20pt;
   font-weight: bold;
@@ -210,6 +241,6 @@ h1 {
   margin: 10px 0px;
   padding: 10px;
 
-}
+}*/
 
 </style>
